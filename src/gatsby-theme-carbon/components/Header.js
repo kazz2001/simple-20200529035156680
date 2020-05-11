b6bb495988f4c164aa91bdcd34e4f15c4bd86c1b
kazz2001 <kazz@planetky.com>
2020-05-11 14:42:01 +0900

    feat: 202005111441

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = props => (
   <Header {...props}>
-    Gatsby theme&nbsp;<span>Carbon</span>
+    Black Music CD Review &nbsp;<span>by planet.ky</span>
   </Header>
 );
 
