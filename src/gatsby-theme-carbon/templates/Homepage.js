d4c287a94f97e21e288798ca0cf18c9da8b73cf0
kazz2001 <kazz@planetky.com>
2020-05-17 13:38:37 +0900

    feat:202005171340

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tag } from 'carbon-components-react';
 import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
 import { calloutLink } from './Homepage.module.scss';
@@ -10,6 +11,9 @@ const FirstLeftText = () => <p>Black Music CD Review</p>;
 const FirstRightText = () => (
     <p>
         <code>Black Music CD Review</code> introduces CD review for Soul and Hip-Hop since 1998
+        <div>
+            <Tag type="blue" title="Clear Filter"> 2019 </Tag>
+        </div>
     </p>
 );
 
